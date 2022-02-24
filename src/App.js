f1c88147bf2ed2242399a412b4c0b7e2f0e96fa7
EkaterinaF <9150221@mail.ru>
2022-02-24 17:36:23 +0200

    lesson-9

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { PersistGate } from "redux-persist/integration/react";
-import { Provider } from "react-redux";
+import { Provider, useSelector, useDispatch } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CustomThemeProvider } from "./theme";
-import { ChatPage, HomePage, ProfilePage, Gists } from "./pages";
+import {
+  ChatPage,
+  HomePage,
+  ProfilePage,
+  Gists,
+  LoginPage,
+  SignUpPage,
+} from "./pages";
 import { store, persistor } from "./store";
+import { PublicRoute, PrivateRoute } from "./components";
+import { sessionSelector, setSessionFB } from "./store/session";
 import "./common.module.css";
 
-
 function App() {
+  const session = useSelector(sessionSelector);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setSessionFB());
+  }, [dispatch]);
+
+  const isAuth = session?.user.email;
   return (
     <React.StrictMode>
       <Provider store={store}>
@@ -16,10 +32,47 @@ function App() {
           <CustomThemeProvider>
             <BrowserRouter>
               <Routes>
-                <Route path="/*" element={<HomePage />} />;
-                <Route path="/chat/*" element={<ChatPage />} />
-                <Route path="/profile/*" element={<ProfilePage />} />
-                <Route path="/gists" element={<Gists />} />
+                <Route path="//*" element={<HomePage session={session} />} />
+                <Route
+                  path="/chat/*"
+                  element={
+                    <PrivateRoute isAuth={isAuth} to="/login">
+                      <ChatPage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/profile/*"
+                  element={
+                    <PrivateRoute isAuth={isAuth}>
+                      <ProfilePage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/gists"
+                  element={
+                    <PrivateRoute isAuth={isAuth}>
+                      <Gists />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/login"
+                  element={
+                    <PublicRoute isAuth={isAuth}>
+                      <LoginPage />
+                    </PublicRoute>
+                  }
+                />
+                <Route
+                  path="/sign-up"
+                  element={
+                    <PublicRoute isAuth={isAuth}>
+                      <SignUpPage />
+                    </PublicRoute>
+                  }
+                />
               </Routes>
             </BrowserRouter>
           </CustomThemeProvider>
--- src/pages/chat.js
+++ src/pages/chat.js
@@ -1,10 +1,13 @@
 import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import { Template, MessageList, ChatList, ChatText } from "../components";
+import { getConversationsFB } from "../store/conversations";
+import { getMessagesFB } from "../store/messages";
 
 export const ChatPage = () => {
   const navigate = useNavigate();
-
+  const dispatch = useDispatch();
   useEffect(() => {
     const listener = ({ code }) => {
       if (code === "Escape") {
@@ -17,6 +20,11 @@ export const ChatPage = () => {
     return () => document.removeEventListener("keydown", listener);
   }, [navigate]);
 
+  useEffect(() => {
+    dispatch(getConversationsFB());
+    dispatch(getMessagesFB());
+  }, [dispatch]);
+
   return (
     <Routes>
       <Route
@@ -25,9 +33,7 @@ export const ChatPage = () => {
       />
       <Route
         path="/:roomId"
-        element={
-          <Template chats={<ChatList />} messages={<MessageList />} />
-        }
+        element={<Template chats={<ChatList />} messages={<MessageList />} />}
       />
     </Routes>
   );
